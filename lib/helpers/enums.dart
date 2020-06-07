f324e47c5169e14b47062f13f3dc9ccd52918f17
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-06-07 22:54:59 +0200

    [Helpers] enums created

---
+++ lib/helpers/enums.dart
@@ -0,0 +1,26 @@
+enum Gender {
+  all,
+  male,
+  female
+}
+
+enum Clothing {
+  all,
+  cloth,
+  nude
+}
+
+enum Pose {
+  all,
+  action,
+  stationary
+}
+
+enum Direction{
+  all,
+  front,
+  back,
+  side
+}
+
+
