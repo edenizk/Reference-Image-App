88f72dfcf29f551928e49ae8bc38c9b68ac13d7c
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-06-12 01:29:28 +0200

    [Pages] Directory changes

--- lib/views/pages/filter_page.dart
+++ lib/views/pages/filter_page.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:reference_photo_app/helpers/enums.dart';
-import 'package:reference_photo_app/pages/slide_page.dart';
+import 'package:reference_photo_app/views/pages/slide_page.dart';
 
 class FilterPage extends StatefulWidget {
   FilterPage({Key key}) : super(key: key);
